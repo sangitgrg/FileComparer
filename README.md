2ce51d799eaf675a1cc048a1c0f275f89d73167f
unknown <tamu.grgsangit@gmail.com>
2021-06-09 00:36:16 +0900

    added comments

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # FileComparer
 
 This program uses pandas and numpy to compare two big excel files and create output with highlight in each cell that are not matched.
+gener_compare.py is the main script for comparing.
+Ignore other files, they are under development.
+
+# Limitation
+
+1. Comaprison should be done by taking one unique key/column from excel as a base for comparison
+2. Row and column count should match between new and old excel file
--- generic_comparer.py
+++ generic_comparer.py
@@ -13,6 +13,7 @@ new_excel_file = config_object['PathConfig']['newFolderPath']
 old_excel_file = config_object['PathConfig']['oldFolderPath']
 new_excel_sheet_name = config_object['ExcelConfig']['newExcelsheetName']
 old_excel_sheet_name = config_object['ExcelConfig']['oldExcelsheetName']
+uniqueKeyForCompare = config_object['ExcelConfig']['uniqueKey']
 
 print('processing ' + new_excel_file)
 # seperating filename and extension
@@ -21,23 +22,31 @@ filename = path.splitext(new_excel_file)[0]
 df_new = pd.read_excel(new_excel_file, sheet_name=new_excel_sheet_name)
 df_old = pd.read_excel(old_excel_file, sheet_name=old_excel_sheet_name)
 
-# getting new tags
+# dropping duplicate record to get only new record
 df_new_tags = pd.concat([df_new, df_old]).drop_duplicates(
-    subset=['Tag No'], keep=False)
-new_tags_list = df_new_tags.index.values.tolist()  # holding new tag numbers
+    subset=[uniqueKeyForCompare], keep=False)
+
+# deleting new record
+# to make old and new same amount of records
+new_tags_list = df_new_tags.index.values.tolist()
 df_new = df_new.drop(new_tags_list)
 
-df_new = df_new[df_new['Tag No'].notna()]  # removing blank rows
-df_old = df_old[df_old['Tag No'].notna()]
+# removing blank rows because after delete there might be null
+df_new = df_new[df_new[uniqueKeyForCompare].notna()]
+df_old = df_old[df_old[uniqueKeyForCompare].notna()]  # just incase
 
 df_new = df_new.reset_index(drop=True)
 df_old = df_old.reset_index(drop=True)
 
 # getting the difference between two
+# comparing with old file for getting new records
+# use unique column/key for comparing
 df_new['StatusMatch'] = np.where(
-    df_new['Status'] == df_old['Status'], 'True', 'False')
+    df_new['Status'] == df_old['Status'], 'True', 'False')   # this will add new column 'StatusMatch'
 df_new['DescriptionMatch'] = np.where(
-    df_new['Description'] == df_old['Description'], 'True', 'False')
+    df_new['Description'] == df_old['Description'], 'True', 'False')  # this will add new column 'DescriptionMatch'
+
+# actual comparison
 difference = df_new[df_new.values != df_old.values]
 # getting row and col index for differences
 rowCol = np.argwhere(difference.notnull().values).tolist()
